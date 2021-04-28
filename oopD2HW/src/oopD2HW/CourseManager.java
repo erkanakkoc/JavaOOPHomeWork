93b69fbcd97d24a4d7f74063ca564ef0099cd949
akkoc.erkan@gmail.com <akkoc.erkan@gmail.com>
2021-04-28 16:58:27 +0300

    D2-HW1

---
+++ oopD2HW/src/oopD2HW/CourseManager.java
@@ -0,0 +1,13 @@
+package oopD2HW;
+
+public class CourseManager {
+	public void addCourse(Course course) {
+		System.out.println(course.name + " Kursu Eklendi!");
+	}
+	
+	public void deleteCourse(Course course) {
+		System.out.println(course.name + " Kursu Silindi!");
+	}
+
+
+}
---
+++ oopD2HW/src/oopD2HW/InstructorManager.java
@@ -0,0 +1,12 @@
+package oopD2HW;
+
+public class InstructorManager {
+	public void addInstructor(Instructor instructor) {
+		System.out.println(instructor.firstName + " " + instructor.firstName + " Ýsimli Eðitmen Eklendi!");
+	}
+	
+	public void deleteCourse(Instructor instructor) {
+		System.out.println(instructor.firstName + " " + instructor.firstName + " Ýsimli Eðitmen Silindi!");
+	}
+
+}
